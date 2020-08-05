997baf54789862d29c556c3e1e27c7b26fde05ea
task4233 <tks.m1205@gmail.com>
2020-08-05 19:05:21 +0900

    add requirements

---
+++ kadai1/cfe/cfe.go
@@ -0,0 +1,13 @@
+// cfe package converts file extention
+// - mandatory
+//   - set root directory
+//   - execute recursively
+// - optional
+//   - arguments
+//     - `--from -f`
+//       - file extention before executing
+//     - `--to -t`
+//       - file extention after executing
+
+package cfe
+
