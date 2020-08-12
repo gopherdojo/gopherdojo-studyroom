d0e54926f2e7afbe80824e95d66c0f110fab4da7
shinnosuke-K <shin.050195@gmail.com>
2020-08-12 20:02:47 +0900

    add: カレントディレクトリのディレクトリとファイル名等を取得する処理を追加

--- kadai1/shinnosuke-K/main.go
+++ kadai1/shinnosuke-K/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -14,5 +16,13 @@ func main() {
 	}
 	dirPath := flag.String("path", pwd, "Directory path to convert image file's extension")
 	flag.Parse()
-	fmt.Println(*dirPath)
+
+	files, err := ioutil.ReadDir(*dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name(), f.IsDir(), filepath.Ext(f.Name()))
+	}
 }
