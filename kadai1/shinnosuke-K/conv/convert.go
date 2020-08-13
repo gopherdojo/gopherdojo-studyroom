37930dda97e8fb933d400351a8554823ae8d2dec
shinnosuke-K <shin.050195@gmail.com>
2020-08-14 01:02:45 +0900

    add: ファイル情報の詳細を取得する処理

--- kadai1/shinnosuke-K/conv/convert.go
+++ kadai1/shinnosuke-K/conv/convert.go
@@ -5,6 +5,9 @@ import (
 )
 
 func Do(dirPath string, before string, after string) {
-	getFiles(dirPath)
+	files := getFiles(dirPath)
+	for n := range files {
+		fmt.Println(files[n])
+	}
 	fmt.Println(before, after)
 }
--- kadai1/shinnosuke-K/conv/files.go
+++ kadai1/shinnosuke-K/conv/files.go
@@ -1,15 +1,20 @@
 package conv
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
 )
 
-func getFiles(path string) {
+type File struct {
+	Dir       string // absolute directory path which files exists
+	Name      string // File Name
+	Extension string
+}
 
-	fmt.Println(path)
+var fileList = make([]File, 0)
+
+func getFiles(path string) []File {
 
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -20,8 +25,12 @@ func getFiles(path string) {
 		if f.IsDir() {
 			getFiles(filepath.Join(path, f.Name()))
 		} else {
-			fmt.Println(f.Name(), f.IsDir(), filepath.Ext(f.Name()))
-
+			fileList = append(fileList, File{
+				Dir:       path,
+				Name:      f.Name(),
+				Extension: filepath.Ext(f.Name())[1:],
+			})
 		}
 	}
+	return fileList
 }
