6215fcc00b0d03cd67ceee1429b2dd7a6bf77e91
shinnosuke-K <shin.050195@gmail.com>
2020-08-15 17:09:33 +0900

    add: ファイルの拡張子を確認する処理

--- kadai1/shinnosuke-K/conv/convert.go
+++ kadai1/shinnosuke-K/conv/convert.go
@@ -2,12 +2,30 @@ package conv
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
+
+var imgExts = []string{"gif", "png", "jpeg"}
 
 func Do(dirPath string, before string, after string) {
 	files := getFiles(dirPath)
 	for n := range files {
 		fmt.Println(files[n])
 	}
-	fmt.Println(before, after)
+
+	err := checkOpt(before, after)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func checkOpt(before string, after string) error {
+	for n := range imgExts {
+		if strings.ToLower(before) != imgExts[n] || strings.ToLower(after) != imgExts[n] {
+			return fmt.Errorf("imgconv: invaild image extension")
+		}
+	}
+	return nil
 }
