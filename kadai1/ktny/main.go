602350c61eddbcdc61078db8bf8233651b964502
ktny <katsunori.t10@gmail.com>
2020-09-24 23:52:14 +0900

    update

--- kadai1/ktny/main.go
+++ kadai1/ktny/main.go
@@ -9,19 +9,17 @@ import (
 )
 
 func main() {
-	var from = flag.String("from", "jpg", "変換前の画像ファイルの拡張子")
-	var to = flag.String("to", "png", "変換後の画像ファイルの拡張子")
+	var from = flag.String("from", "jpg", "Image file extension before conversion")
+	var to = flag.String("to", "png", "Image file extension after conversion")
 
 	flag.Parse()
 
 	targetDir := flag.Arg(0)
 	if targetDir == "" {
-		fmt.Println("ディレクトリが指定されていません")
+		fmt.Println("Directory is not defined.")
 		os.Exit(1)
 	}
 	targetDir = strings.TrimRight(targetDir, "/")
-
-	// println(targetDir, *from, *to)
 
 	// targetDir配下を再帰的に画像変換する
 	filepaths := util.DirWalk(targetDir)
@@ -32,11 +30,15 @@ func main() {
 			if strings.HasSuffix(filepath, ".jpg") || strings.HasSuffix(filepath, ".jpeg") {
 				fmt.Printf("convert %s\n", filepath)
 				util.ConvertImage(filepath, *from, *to)
+			} else {
+				fmt.Printf("[Warn]%s was not converted. It is %s file.\n", filepath, *from)
 			}
 		case "png":
 			if strings.HasSuffix(filepath, ".png") {
 				fmt.Printf("convert %s\n", filepath)
 				util.ConvertImage(filepath, *from, *to)
+			} else {
+				fmt.Printf("[Warn]%s was not converted. It is %s file.\n", filepath, *from)
 			}
 		}
 	}
