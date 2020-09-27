ccde97766a12de8e5901c86011e39da5d96206b2
ktny <katsunori.t10@gmail.com>
2020-09-27 16:29:18 +0900

    update

--- kadai1/ktny/main.go
+++ kadai1/ktny/main.go
@@ -9,21 +9,28 @@ import (
 )
 
 func main() {
-	var from = flag.String("from", "jpg", "Image file extension before conversion")
-	var to = flag.String("to", "png", "Image file extension after conversion")
+	var from = flag.String("from", "jpg", "extension before conversion")
+	var to = flag.String("to", "png", "extension after conversion")
 
 	flag.Parse()
 
+	// ディレクトリが指定されていない場合は終了する
 	targetDir := flag.Arg(0)
 	if targetDir == "" {
-		fmt.Println("Directory is not defined.")
+		fmt.Println("[Error]Directory is not defined.")
 		os.Exit(1)
 	}
 	targetDir = strings.TrimRight(targetDir, "/")
 
+	// 指定した変換前後の拡張子が同じ場合は終了する
+	if *from == *to {
+		fmt.Println("[Error]from and to extension is same.")
+		os.Exit(1)
+	}
+
 	// targetDir配下を再帰的に画像変換する
 	filepaths := util.DirWalk(targetDir)
-	fmt.Printf("targetDir: %s, from: %s, to: %s\n", targetDir, *from, *to)
+	fmt.Printf("[Info]from=%s, to=%s, targetDir=%s\n", *from, *to, targetDir)
 	for _, filepath := range filepaths {
 		switch *from {
 		case "jpg", "jpeg":
