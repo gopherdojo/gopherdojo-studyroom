3753ee4f1949914ebd67e8cff2aa57a9105371b9
yuonoda <yuonoa95@gmail.com>
2020-12-06 12:13:22 +0900

    flagで変換前後のフォーマットを受け取れるようにした

--- kadai1/yuonoda/main.go
+++ kadai1/yuonoda/main.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"image/jpeg"
 	"image/png"
 	"io/ioutil"
 	"log"
 )
 
+var fromFmt = flag.String("from", "jpg", "Specify a format of your original image")
+var toFmt = flag.String("to", "png", "Specify a format which you want to convert the image to")
+
 func main() {
+	flag.Parse()
+	fmt.Printf("fromFmt: %s\n", *fromFmt)
+	fmt.Printf("toFmt: %s\n", *toFmt)
 
-	imageBytes, err := ioutil.ReadFile("./kadai1/gopher.jpg")
+	// 元画像を読み込み
+	imageBytes, err := ioutil.ReadFile("./gopher.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	buffer := bytes.NewReader(imageBytes)
+	jpgImg, err := jpeg.Decode(buffer)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	img, err := jpeg.Decode(bytes.NewReader(imageBytes))
-	if err != nil {
+	// 変換
+	newBuf := new(bytes.Buffer)
+	if err != png.Encode(newBuf, jpgImg) {
 		log.Fatal(err)
 	}
 
-	buf := new(bytes.Buffer)
-	if err != png.Encode(buf, img) {
-		log.Fatal(err)
-	}
-
-	//log.Println(reflect.TypeOf(buf))
-	ioutil.WriteFile("gopher.png", buf.Bytes(), 0644)
+	// ファイル出力
+	ioutil.WriteFile("gopher.png", newBuf.Bytes(), 0644)
 
 }
