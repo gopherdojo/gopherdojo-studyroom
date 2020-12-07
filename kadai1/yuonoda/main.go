0d10587ef1d684134851b71bd366d50a7cd9b7dd
yuonoda <yuonoa95@gmail.com>
2020-12-07 22:06:22 +0900

    ディレクトリ内の画像を取得する関数を追加

--- kadai1/yuonoda/main.go
+++ kadai1/yuonoda/main.go
@@ -9,16 +9,55 @@ import (
 	"image/png"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 var fromFmt = flag.String("from", "jpg", "Specify a format of your original image")
 var toFmt = flag.String("to", "png", "Specify a format which you want to convert the image to")
+var path = flag.String("path", ".", "Path to a directory in which images will be converted recursively")
+
+func getImages(path string, fmt string) ([]os.FileInfo, error) { // TODO 何を返すべきか
+	// ディレクトリ内を再帰的に探索
+	var files []os.FileInfo
+	err := filepath.Walk(path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+
+			// 対象の拡張子か査定
+			ext := filepath.Ext(path)[1:]
+			if ext != fmt { // TODO jpgの表記
+				return nil
+			}
+			files = append(files, info)
+			return nil
+		})
+
+	// エラーがあれば、エラーを返す
+	if err != nil {
+		return []os.FileInfo{}, err
+	}
+	return files, nil
+}
 
 func main() {
 	flag.Parse()
 	fmt.Printf("fromFmt: %s\n", *fromFmt)
 	fmt.Printf("toFmt: %s\n", *toFmt)
+	fmt.Printf("path: %s\n", *path)
+	fmt.Println("-------")
 
+	files, err := getImages(*path, *fromFmt)
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+
+	return
 	// 元画像を読み込み
 	imageBytes, err := ioutil.ReadFile("./gopher.jpg")
 	if err != nil {
