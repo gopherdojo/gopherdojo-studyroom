1e1ea9320ce71b44b3dd9cd56c9eeb3091b410ee
yuonoda <yuonoda95@gmail.com>
2021-01-11 12:01:06 +0900

    エンコードのテストを作成

--- kadai1/yuonoda/lib/img_test.go
+++ kadai1/yuonoda/lib/img_test.go
@@ -4,50 +4,60 @@ import (
 	"bytes"
 	"image"
 	"image/color"
+	"image/gif"
+	"image/jpeg"
 	"image/png"
 	"testing"
 )
 
 func TestEncode(t *testing.T) {
-	t.Logf("TestImgEncode")
+	t.Logf("TestEncode")
 
 	// 生画像を生成
-	image := generateImage(t)
+	image := testGenerateImage(t)
 
-	// エンコード
-	ic := imgConverter{"test", image}
-	buff1 := bytes.NewBuffer([]byte{})
-	err := ic.encode(buff1, "png")
-	if err != nil {
-		t.Errorf("failed to encode err:%v", err)
+	// サンプルデータでループ
+	fmts := []string{"png", "gif", "jpg"}
+	for _, fmt := range fmts {
+
+		// エンコード
+		ic := imgConverter{"test", image}
+		buff1 := bytes.NewBuffer([]byte{})
+		err := ic.encode(buff1, fmt)
+		if err != nil {
+			t.Errorf("failed to encode err:%v", err)
+		}
+
+		// imgConverterを使わずにエンコード
+		buff2 := bytes.NewBuffer([]byte{})
+		switch fmt {
+		case "png":
+			err = png.Encode(buff2, image)
+			break
+		case "jpg":
+			err = jpeg.Encode(buff2, image, nil)
+			break
+		case "gif":
+			err = gif.Encode(buff2, image, nil)
+			break
+		}
+		if err != nil {
+			t.Errorf("failed to test encode err:%v ", err)
+		}
+
+		// 双方のエンコード結果が一致するか検証
+		if bytes.Compare(buff1.Bytes(), buff2.Bytes()) != 0 {
+			t.Errorf("encode result mismatch")
+		}
 	}
 
-	// imgConverterを使わずにエンコード
-	buff2 := bytes.NewBuffer([]byte{})
-	err = png.Encode(buff2, image)
-	if err != nil {
-		t.Errorf("failed to test encode err:%v ", err)
-	}
-
-	// 双方のエンコード結果が一致するか検証
-	if bytes.Compare(buff1.Bytes(), buff2.Bytes()) != 0 {
-		t.Errorf("encode result mismatch")
-	}
-
-	//// 目視用にファイル出力
-	//f, _ := os.Create("test1.png")
-	//f.Write(buff1.Bytes())
-	//
-	//
-	//// 目視用にファイル出力
-	//f, _ = os.Create("test2.png")
-	//f.Write(buff2.Bytes())
 	return
 }
 
 // インメモリイメージを作成する
-func generateImage(t *testing.T) image.Image {
+func testGenerateImage(t *testing.T) image.Image {
 	t.Log("generateImage")
+	t.Helper()
 	// 画像を作成
 	w := 200
 	h := 100
