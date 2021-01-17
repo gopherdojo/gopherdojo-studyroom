4ef1efb328817ce79268bbbedd3118cd01f758ce
yuonoda <yuonoda95@gmail.com>
2021-01-18 08:30:36 +0900

    GETリクエスト実装

--- kadai3-2/yuonoda/lib/splitDownload.go
+++ kadai3-2/yuonoda/lib/splitDownload.go
@@ -1,7 +1,33 @@
 package splitDownload
 
-import "log"
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func Run() {
 	log.Println("Run")
+
+	// リクエストとクライアントの作成
+	url := "https://yahoo.co.jp"
+	r := bytes.NewReader([]byte{})
+	req, err := http.NewRequest("GET", url, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{}
+
+	// リクエストの実行
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("body:%s", body)
 }
