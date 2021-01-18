63e44946a184c1e50f484d6b58b90dafc3f17e33
yuonoda <yuonoda95@gmail.com>
2021-01-18 21:40:06 +0900

    HEADでコンテンツサイズを取得

--- kadai3-2/yuonoda/lib/splitDownload.go
+++ kadai3-2/yuonoda/lib/splitDownload.go
@@ -2,18 +2,45 @@ package splitDownload
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 )
+
+// コンテンツのデータサイズを取得
+func getContentSize(url string) (size int, err error) {
+	// HEADでサイズを調べる
+	res, err := http.Head(url)
+	if err != nil {
+		return 0, err
+	}
+
+	// データサイズを取得
+	header := res.Header
+	cl, ok := header["Content-Length"]
+	if !ok {
+		return 0, errors.New("Content-Length couldn't be found")
+	}
+	size, err = strconv.Atoi(cl[0])
+	return
+}
 
 func Run() {
 	log.Println("Run")
 
-	// リクエストとクライアントの作成
+	// コンテンツのデータサイズを取得
 	url := "https://dumps.wikimedia.org/jawiki/20210101/jawiki-20210101-pages-articles-multistream-index.txt.bz2"
+	size, err := getContentSize(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("size: %d", size)
+
+	// リクエストとクライアントの作成
 	r := bytes.NewReader([]byte{})
 	req, err := http.NewRequest("GET", url, r)
 	if err != nil {
@@ -26,6 +53,8 @@ func Run() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// bodyの取得
 	body, err := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
 	if err != nil {
