52e70e6ab0debb8701f952f1477fb410e16b01db
yuonoda <yuonoda95@gmail.com>
2021-01-19 22:07:55 +0900

    逐次アクセスでまず実装

--- kadai3-2/yuonoda/lib/splitDownload.go
+++ kadai3-2/yuonoda/lib/splitDownload.go
@@ -3,8 +3,10 @@ package splitDownload
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -38,38 +40,61 @@ func Run() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("size: %d", size)
-
-	// リクエストとクライアントの作成
-	r := bytes.NewReader([]byte{})
-	req, err := http.NewRequest("GET", url, r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client := &http.Client{}
-
-	// リクエストの実行
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// bodyの取得
-	body, err := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("size: %d\n", size)
 
 	// ファイルの作成
 	_, filename := filepath.Split(url)
-	file, err := os.Create(filename + ".download")
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filePath := homedir + "/Downloads/" + filename
+	log.Println(filePath)
+	file, err := os.Create(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// 1MBごとにアクセス
+	singleSize := 1000000
+	count := int(math.Ceil(float64(size) / float64(singleSize)))
+	var fileData []byte
+	log.Printf("count: %d\n", count)
+	for i := 0; i < count; i++ {
+
+		// 担当する範囲を決定
+		startByte := singleSize*i + 1
+		endByte := singleSize * (i + 1)
+		rangeVal := fmt.Sprintf("bytes=%d-%d", startByte, endByte)
+		log.Println("rangeVal:", rangeVal)
+
+		// リクエストとクライアントの作成
+		r := bytes.NewReader([]byte{})
+		req, err := http.NewRequest("GET", url, r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Range", rangeVal)
+		client := &http.Client{}
+
+		// リクエストの実行
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// bodyの取得
+		body, err := ioutil.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fileData = append(fileData, body...)
+		log.Printf("len(fileData)(MB) : %d", len(fileData)/singleSize)
+	}
+
 	// データの書き込み
-	_, err = file.Write(body)
+	_, err = file.Write(fileData)
 	if err != nil {
 		log.Fatal(err)
 	}
