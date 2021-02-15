7abc3de88a0df494b6fe44fd942e58ec827daae8
Ryo Nakamine <rnakamine8080@gmail.com>
2021-02-15 23:06:41 +0900

    Add directory walk

--- kadai1/cli.go
+++ kadai1/cli.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
 )
 
-const ExitCodeOK = 1
+const (
+	ExitCodeOK             = 0
+	ExitCodeError          = 1
+	ExitCodeParseFlagError = 1
+)
 
 type CLI struct {
 	outStream, errStream io.Writer
 }
 
 func (c *CLI) Run(args []string) int {
-	// flags := flag.NewFlagSet("imgconv", flag.ContinueOnError)
+	var dir string
+	var from string
+	var target []string
 
-	fmt.Fprint(c.outStream, "Do awesome work\n")
+	flags := flag.NewFlagSet("imgconv", flag.ContinueOnError)
+	flags.SetOutput(c.errStream)
+	flags.StringVar(&dir, "dir", "", "")
+	flags.StringVar(&from, "from", "jpg", "")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeParseFlagError
+	}
+
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if filepath.Ext(path) == "."+from {
+				target = append(target, path)
+			}
+			return nil
+		})
+	if err != nil {
+		return ExitCodeError
+	}
+
+	fmt.Println(target)
 
 	return ExitCodeOK
 }
