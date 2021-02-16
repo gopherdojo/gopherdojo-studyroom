55c190221341976ab6dcce2fb2334741bed49b32
Ryo Nakamine <rnakamine8080@gmail.com>
2021-02-17 05:49:00 +0900

    Refactor

--- kadai1/cli.go
+++ kadai1/cli.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"flag"
-	"image"
 	_ "image/jpeg"
-	"image/png"
 	"io"
-	"os"
-	"path/filepath"
+
+	"./imgconv"
 )
 
 const (
@@ -28,6 +26,7 @@ func (c *CLI) Run(args []string) int {
 
 	flags := flag.NewFlagSet("imgconv", flag.ContinueOnError)
 	flags.SetOutput(c.errStream)
+
 	flags.StringVar(&dir, "dir", "", "")
 	flags.StringVar(&from, "from", "jpg", "")
 	flags.StringVar(&to, "to", "png", "")
@@ -36,46 +35,16 @@ func (c *CLI) Run(args []string) int {
 		return ExitCodeError
 	}
 
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == "."+from {
-			err = convertImage(path, to)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
+	images, err := imgconv.FileWalk(dir, from, to)
 	if err != nil {
 		return ExitCodeError
 	}
+	for _, img := range images {
+		err := img.Convert()
+		if err != nil {
+			return ExitCodeError
+		}
+	}
 
 	return ExitCodeOK
-}
-
-func convertImage(path string, ext string) error {
-	sf, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer sf.Close()
-
-	img, _, err := image.Decode(sf)
-	if err != nil {
-		return err
-	}
-
-	df, _ := os.Create(path[:len(path)-len(filepath.Ext(path))] + "." + ext)
-	defer df.Close()
-
-	err = png.Encode(df, img)
-	if err != nil {
-		return err
-	}
-
-	err = os.Remove(path)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
