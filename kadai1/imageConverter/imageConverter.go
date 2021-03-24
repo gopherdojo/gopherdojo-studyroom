ff541bd9fcc3bcb1fb1d4da891d4122bb4bcc5fe
dnakano <dnakano@student.42tokyo.jp>
2021-03-24 18:41:12 +0900

    Add: imageConverter.Png2jpg

--- kadai1/imageConverter/imageConverter.go
+++ kadai1/imageConverter/imageConverter.go
@@ -40,3 +40,35 @@ func Jpg2png(filename string) {
 	}
 	fs.Close()
 }
+
+func Png2jpg(filename string) {
+	fileext := strings.ToLower(filepath.Ext(filename))
+	if fileext != ".png" {
+		return
+	}
+
+	fs, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	img, err := png.Decode(fs)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fs.Close()
+
+	fs, err = os.OpenFile(filename+".jpg", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	err = jpeg.Encode(fs, img, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	fs.Close()
+}
