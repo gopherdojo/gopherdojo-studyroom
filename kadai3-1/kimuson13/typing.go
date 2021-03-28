58cda9bfa57f047d7855d6c068c672f822436c6c
kimuson <kimuson13@basket.com>
2021-03-28 20:18:13 +0900

    Created typing game and added wordlist

--- kadai3-1/kimuson13/typing.go
+++ kadai3-1/kimuson13/typing.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"math/rand"
 	"os"
 	"time"
@@ -11,6 +13,39 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	q := rand.Intn(len(word.WordList))
-	fmt.Fprintf(os.Stdout, word.WordList[q])
+	quiz := rand.Perm(len(word.WordList))
+	var correct int
+	ch1 := input(os.Stdin)
+	ch2 := time.After(20 * time.Second)
+	fmt.Print("You need type word is displayed as question\nTime limits is 20 seconds\n")
+	for _, q := range quiz {
+		fmt.Print("question:\n")
+		fmt.Print(word.WordList[q] + "\n")
+		fmt.Print("input:\n")
+		select {
+		case r := <-ch1:
+			if r == word.WordList[q] {
+				fmt.Print("correct!\n")
+				correct++
+			} else {
+				fmt.Print("incorrect!\n")
+			}
+		case <-ch2:
+			fmt.Print("\ntimed out!\n")
+			fmt.Printf("Your result: %v", correct)
+			os.Exit(0)
+		}
+	}
+}
+
+func input(r io.Reader) <-chan string {
+	ch1 := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch1 <- s.Text()
+		}
+		close(ch1)
+	}()
+	return ch1
 }
