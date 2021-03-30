9ff37b1c32bae4d2fd3911cc6a2d744ea8e64f8a
kassy11 <kotaro11kassy@gmail.com>
2021-03-30 18:29:21 +0900

    kadai1-kassy11 変換画像に関する構造体を定義して、変換関数をリファクタリング

--- kadai1/kassy11/convert/convert.go
+++ kadai1/kassy11/convert/convert.go
@@ -9,28 +9,35 @@ import (
 	"path/filepath"
 )
 
-func Do(dir string, outdir string, inputFormat string, outputFormat string) {
-	err := os.Mkdir(outdir, 0777)
+type ConvertImage struct {
+	InputFormat     string
+	OutputFormat    string
+	InputDirectory  string
+	OutputDirectory string
+}
+
+func (img *ConvertImage) Do() {
+	err := os.Mkdir(img.OutputDirectory, 0777)
 	logError(err, "cannot create directory")
-	error := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == "."+inputFormat {
+	error := filepath.Walk(img.InputDirectory, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == "."+img.InputFormat {
 			file, err := os.Open(path)
 			logError(err, "Cannot open file")
 			defer file.Close()
-			img, _, err := image.Decode(file)
+			convertImage, _, err := image.Decode(file)
 			logError(err, "Failed to convert file to image")
-			if outputFormat == format.Png {
-				dstPath := filepath.Join(outdir, getFileNameWithoutExt(path)+".png")
+			if img.OutputFormat == format.Png {
+				dstPath := filepath.Join(img.OutputDirectory, getFileNameWithoutExt(path)+".png")
 				out, err := os.Create(dstPath)
 				logError(err, "Failed to create destination path")
 				defer out.Close()
-				png.Encode(out, img)
-			} else if outputFormat == format.Jpg || outputFormat == format.Jpeg {
-				dstPath := filepath.Join(outdir, getFileNameWithoutExt(path)+".jpg")
+				png.Encode(out, convertImage)
+			} else if img.OutputFormat == format.Jpg || img.OutputFormat == format.Jpeg {
+				dstPath := filepath.Join(img.OutputDirectory, getFileNameWithoutExt(path)+".jpg")
 				out, err := os.Create(dstPath)
 				logError(err, "Failed to create destination path")
 				defer out.Close()
-				jpeg.Encode(out, img, nil)
+				jpeg.Encode(out, convertImage, nil)
 			}
 		}
 		if err != nil {
@@ -40,5 +47,5 @@ func Do(dir string, outdir string, inputFormat string, outputFormat string) {
 	})
 	logError(error, "Error on filepath.Walk")
 	fmt.Println("Succuessfully convert image files")
-	fmt.Printf("Check %s directory\n", outdir)
+	fmt.Printf("Check %s directory\n", img.OutputDirectory)
 }
--- kadai1/kassy11/main.go
+++ kadai1/kassy11/main.go
@@ -8,13 +8,6 @@ import (
 	"github.com/kassy11/gopherdojo-studyroom/kadai1/kassy11/convert"
 )
 
-type ConvertImage struct {
-	inputFormat     string
-	outputFormat    string
-	inputDirectory  string
-	outputDirectory string
-}
-
 var format *convert.FormatType
 
 func init() {
@@ -22,18 +15,20 @@ func init() {
 }
 
 func main() {
+	var img convert.ConvertImage
+
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage: imgconv[options..] <directory>")
 		fmt.Fprintln(os.Stderr, "Options")
 		flag.PrintDefaults()
 	}
 
-	var inputFormat string
-	var outputFormat string
-	var outputDirectory string
-	flag.StringVar(&inputFormat, "i", format.Jpg, "-i <format(jpg or jpeg or png))>")
-	flag.StringVar(&outputFormat, "f", format.Png, "-f <format(jpg or jpeg or png)>")
-	flag.StringVar(&outputDirectory, "o", "output", "-o <directory name>")
+	var input string
+	var output string
+	var outputDir string
+	flag.StringVar(&input, "i", format.Jpg, "-i <format(jpg or jpeg or png))>")
+	flag.StringVar(&output, "f", format.Png, "-f <format(jpg or jpeg or png)>")
+	flag.StringVar(&outputDir, "o", "output", "-o <directory name>")
 	flag.Parse()
 
 	if len(flag.Args()) <= 0 {
@@ -44,14 +39,16 @@ func main() {
 
 	dir := flag.Arg(0)
 
-	if (inputFormat != format.Jpg && inputFormat != format.Jpeg && inputFormat != format.Png) || (outputFormat != format.Jpg && outputFormat != format.Jpeg && outputFormat != format.Png) {
+	if (input != format.Jpg && input != format.Jpeg && input != format.Png) || (output != format.Jpg && output != format.Jpeg && output != format.Png) {
 		fmt.Fprintln(os.Stderr, "imgconv: Invalid image format")
 		fmt.Fprintln(os.Stderr, "imgconv: try 'imgconv--help' for more information")
 		os.Exit(1)
-	} else if inputFormat == outputFormat {
+	} else if input == output {
 		fmt.Fprintln(os.Stderr, "imgconv: Incorrect parameter combination")
 		fmt.Fprintln(os.Stderr, "imgconv: ry 'imgconv--help' for more information")
 		os.Exit(1)
 	}
-	convert.Do(dir, outputDirectory, inputFormat, outputFormat)
+
+	img = convert.ConvertImage{InputFormat: input, OutputFormat: output, InputDirectory: dir, OutputDirectory: outputDir}
+	img.Do()
 }
