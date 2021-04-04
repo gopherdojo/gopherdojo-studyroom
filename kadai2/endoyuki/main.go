c019047cbc7b0c596dbc09b58d177d196e6b7059
endo-yuki <engineer@endo-yuki.net>
2021-04-04 11:39:23 +0900

    Fix struct name

--- kadai2/endoyuki/main.go
+++ kadai2/endoyuki/main.go
@@ -9,18 +9,18 @@ import (
 	"endoyuki/conversion"
 )
 
-type Hoge struct {
+type Info struct {
 	inputDir  string //指定するディレクトリ
 	outputDir string //出力先ディレクトリ
 	beforeExt string //変換前の拡張子
 	afterExt  string //変換後の拡張子
 }
 
-var hoge Hoge
+var info Info
 
 func main() {
-	hoge.beforeExt = *flag.String("b", "jpg", "before Extension")
-	hoge.afterExt = *flag.String("a", "png", "after Extension")
+	info.beforeExt = *flag.String("b", "jpg", "before Extension")
+	info.afterExt = *flag.String("a", "png", "after Extension")
 	flag.Parse()
 
 	args := flag.Args()
@@ -34,10 +34,10 @@ func main() {
 			log.Fatal(err)
 		}
 	}
-	hoge.inputDir = args[0]
-	hoge.outputDir = args[1]
+	info.inputDir = args[0]
+	info.outputDir = args[1]
 
-	err := conversion.Convert(hoge.inputDir, hoge.outputDir, hoge.beforeExt, hoge.afterExt)
+	err := conversion.Convert(info.inputDir, info.outputDir, info.beforeExt, info.afterExt)
 	if err != nil {
 		log.Fatal(err)
 	}
