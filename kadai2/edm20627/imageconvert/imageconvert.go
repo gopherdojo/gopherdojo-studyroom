183f52ff23d7838fba5d3b909d7a7c7dea9d6ae4
edm20627 <eng00627@gmail.com>
2021-04-07 15:00:19 +0900

    課題1をコピー

---
+++ kadai2/edm20627/imageconvert/imageconvert.go
@@ -0,0 +1,108 @@
+package imageconvert
+
+import (
+	"errors"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+// Supported extensions.
+var SupportedFormat = []string{"png", "jpg", "jpeg", "gif"}
+
+type ConvertImage struct {
+	filepaths    []string
+	From, To     string
+	DeleteOption bool
+}
+
+// Get the target files for image conversion.
+func (ci *ConvertImage) Get(dirs []string) error {
+	for _, dir := range dirs {
+		err := filepath.Walk(dir,
+			func(path string, info os.FileInfo, err error) error {
+				if info == nil {
+					return errors.New(path + " is not directory")
+				}
+				if info.IsDir() || filepath.Ext(path)[1:] != ci.From {
+					return nil
+				}
+				ci.filepaths = append(ci.filepaths, path)
+				return nil
+			})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Perform image conversion.
+func (ci *ConvertImage) Convert() error {
+	for _, path := range ci.filepaths {
+		err := convert(path, ci.To, ci.DeleteOption)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Check for supported extensions.
+func (ci *ConvertImage) Valid() bool {
+	for _, v := range SupportedFormat {
+		if v == ci.From {
+			for _, v := range SupportedFormat {
+				if v == ci.To {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func convert(path string, to string, deleteOption bool) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(path[:len(path)-len(filepath.Ext(path))+1] + to)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	switch to {
+	case "png":
+		if err := png.Encode(out, img); err != nil {
+			return err
+		}
+	case "jpg", "jpeg":
+		if err := jpeg.Encode(out, img, nil); err != nil {
+			return err
+		}
+	case "gif":
+		if err := gif.Encode(out, img, nil); err != nil {
+			return err
+		}
+	}
+
+	if deleteOption {
+		if err := os.Remove(path); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
