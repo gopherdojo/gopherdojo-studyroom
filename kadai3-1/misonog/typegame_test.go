ef2dc869b8d552af00c569821d81ea2a2e51b0bc
Gimpei Misono <ginpeimisonou@gmail.com>
2021-04-15 23:24:30 +0900

    Add run test

--- kadai3-1/misonog/typegame_test.go
+++ kadai3-1/misonog/typegame_test.go
@@ -5,8 +5,28 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
 )
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	in := make(chan string)
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		in <- "rust"
+		time.Sleep(1 * time.Second)
+	}()
+	out := new(bytes.Buffer)
+	run(in, out)
+
+	expected := "rust"
+	actual := strings.TrimRight(out.String(), "r\n")
+	if actual != expected {
+		t.Errorf(`expected="%s" actual="%s"`, expected, actual)
+	}
+}
 
 func TestInput(t *testing.T) {
 	t.Parallel()
