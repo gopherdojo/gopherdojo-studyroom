e4ba981c99c24dd66c03cb837c9ce8650a36203d
Gimpei Misono <ginpeimisonou@gmail.com>
2021-04-18 17:01:33 +0900

    add pdownload constructor

--- kadai3-2/misonog/pdownload.go
+++ kadai3-2/misonog/pdownload.go
@@ -1,27 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
+// Pdownload structs
+type Pdownload struct {
+	Utils
+	URL       string
+	TargetDir string
+}
 
-func download(filename string, dirname string, url string) error {
-	res, err := http.Get(url)
-	if err != nil {
-		return err
+func New() *Pdownload {
+	return &Pdownload{
+		Utils: &Data{},
 	}
-	defer res.Body.Close()
-
-	filepath := fmt.Sprintf("%s/%s", dirname, filename)
-
-	out, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	io.Copy(out, res.Body)
-	return nil
 }
