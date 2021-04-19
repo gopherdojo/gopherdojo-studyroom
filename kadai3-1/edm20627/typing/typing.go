306a7b5c008f0836d55e7032c21e563e7726e821
edm20627 <eng00627@gmail.com>
2021-04-20 01:01:47 +0900

    初期実装

---
+++ kadai3-1/edm20627/typing/typing.go
@@ -0,0 +1,43 @@
+package typing
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"time"
+)
+
+func Start(r io.Reader, w io.Writer, words []string, gameTime time.Duration) {
+	var score int
+	timeLimit := time.After(gameTime)
+	ch := input(r)
+	fmt.Fprintln(w, "game start!!")
+
+L:
+	for _, word := range words {
+		fmt.Fprintln(w, word)
+		fmt.Fprint(w, ">")
+		select {
+		case answer := <-ch:
+			if word == answer {
+				score++
+			}
+		case <-timeLimit:
+			fmt.Fprintf(w, "\ntime out!!\n")
+			break L
+		}
+	}
+	fmt.Fprintln(w, "score: ", score)
+}
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+	return ch
+}
