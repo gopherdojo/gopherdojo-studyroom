539b81a21b40279fbc92c5af4bc052a0928d1a77
edm20627 <eng00627@gmail.com>
2021-05-03 03:19:28 +0900

    fix テスト用画像を使用しテストできるようにtastdataを作成

--- kadai2/edm20627/imageconvert/imageconvert_test.go
+++ kadai2/edm20627/imageconvert/imageconvert_test.go
@@ -1,12 +1,6 @@
 package imageconvert_test
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
-	"os"
 	"reflect"
 	"testing"
 
@@ -21,19 +15,9 @@ func TestGet(t *testing.T) {
 		from      string
 		expected  []string
 	}{
-		{name: "get jpg", dirs: []string{"../testdata"}, from: "jpg", expected: []string{"../testdata/hoge/img_2.jpg", "../testdata/img_1.jpg"}},
-		{name: "get png", dirs: []string{"../testdata"}, from: "png", expected: []string{"../testdata/hoge/img_2.png", "../testdata/img_1.png"}},
-		{name: "get gif", dirs: []string{"../testdata"}, from: "gif", expected: []string{"../testdata/hoge/img_2.gif", "../testdata/img_1.gif"}},
-	}
-
-	for _, c := range success_cases {
-		for _, path := range c.expected {
-			err := testCreateImage(t, path, c.from)
-			if err != nil {
-				t.Fatal("Failed to create the test image")
-			}
-			defer testRemoveTestdata(t)
-		}
+		{name: "get jpg", dirs: []string{"../testdata"}, from: "jpg", expected: []string{"../testdata/hoge/img_1.jpg", "../testdata/img_1.jpg"}},
+		{name: "get png", dirs: []string{"../testdata"}, from: "png", expected: []string{"../testdata/hoge/img_2.png", "../testdata/img_2.png"}},
+		{name: "get gif", dirs: []string{"../testdata"}, from: "gif", expected: []string{"../testdata/hoge/img_3.gif", "../testdata/img_3.gif"}},
 	}
 
 	for _, c := range success_cases {
@@ -77,19 +61,12 @@ func TestConvert(t *testing.T) {
 		to        string
 	}{
 		{name: "jpg to png", filepaths: []string{"../testdata/img_1.jpg"}, from: "jpg", to: "png"},
-		{name: "png to gif", filepaths: []string{"../testdata/img_1.png"}, from: "png", to: "gif"},
-		{name: "gif to jpg", filepaths: []string{"../testdata/img_1.gif"}, from: "gif", to: "jpg"},
+		{name: "png to gif", filepaths: []string{"../testdata/img_2.png"}, from: "png", to: "gif"},
+		{name: "gif to jpg", filepaths: []string{"../testdata/img_3.gif"}, from: "gif", to: "jpg"},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			for _, path := range c.filepaths {
-				err := testCreateImage(t, path, c.from)
-				if err != nil {
-					t.Fatal("Failed to create the test image")
-				}
-				defer testRemoveTestdata(t)
-			}
 			ci := imageconvert.ConvertImage{Filepaths: c.filepaths, To: c.to}
 			if actual := ci.Convert(); actual != nil {
 				t.Error(actual)
@@ -122,54 +99,4 @@ func TestValid(t *testing.T) {
 			}
 		})
 	}
-}
-
-func testCreateImage(t *testing.T, path string, fmt string) error {
-	t.Helper()
-
-	err := os.MkdirAll("../testdata/hoge", 0777)
-	if err != nil {
-		t.Error(err)
-	}
-
-	file, err := os.Create(path)
-	if err != nil {
-		t.Error(err)
-	}
-	defer file.Close()
-
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	for i := img.Rect.Min.Y; i < img.Rect.Max.Y; i++ {
-		for j := img.Rect.Min.X; j < img.Rect.Max.X; j++ {
-			img.Set(j, i, color.RGBA{255, 255, 0, 0})
-		}
-	}
-
-	switch fmt {
-	case "png":
-		if err := png.Encode(file, img); err != nil {
-			return err
-		}
-	case "jpg", "jpeg":
-		if err := jpeg.Encode(file, img, nil); err != nil {
-			return err
-		}
-	case "gif":
-		if err := gif.Encode(file, img, nil); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func testRemoveTestdata(t *testing.T) error {
-	t.Helper()
-
-	err := os.RemoveAll("../testdata")
-	if err != nil {
-		t.Error(err)
-	}
-
-	return nil
 }
