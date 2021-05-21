f4b0ddd9dde99df27cb79283b9d3f98a2e8e4434
MizushimaToshihiko <pandachan5228@gmail.com>
2021-05-21 09:14:00 +0900

    main.go,pic-conv/picconvert.goのリファクタリング go fmtの適用 処理完了後のメッセージ出力

--- kadai1/Mizushima/main.go
+++ kadai1/Mizushima/main.go
@@ -10,48 +10,48 @@ var preFormat string
 var afterFormat string
 
 func init() {
-  flag.StringVar(&preFormat, "pre", "jpeg", "the file format before conversion")
-  flag.StringVar(&afterFormat, "post", "png", "the file format after conversion")
-  flag.Parse()
+	flag.StringVar(&preFormat, "pre", "jpeg", "the file format before conversion")
+	flag.StringVar(&afterFormat, "post", "png", "the file format after conversion")
+	flag.Parse()
 }
 
 func isCorresponds(format string) bool {
-  ext := []string{"jpg", "jpeg", "png", "gif"}
+	ext := []string{"jpg", "jpeg", "png", "gif"}
 
-  var flag bool = false
+	var flag bool = false
 
-  for _, e := range ext {
-    if e == format {
-      flag = true;
-    }
-  }
+	for _, e := range ext {
+		if e == format {
+			flag = true
+		}
+	}
 
-  if !flag {
-    return false
-  }
-  return true
+	if !flag {
+		return false
+	}
+	return true
 }
 
 // errHandler occur errors.
 func errHandler() {
-  if preFormat == afterFormat {
-    log.Fatal("arguments of -pre and -post are same.")
-  }
+	if preFormat == afterFormat {
+		log.Fatal("arguments of -pre and -post are same.")
+	}
 
-  if len(flag.Args()) == 0 {
-    log.Fatal("please input the path.")
-  }
+	if len(flag.Args()) == 0 {
+		log.Fatal("please input the path.")
+	}
 
-  if !isCorresponds(preFormat) {
-    log.Fatal(preFormat, " is not supported.")
-  } else if !isCorresponds(afterFormat) {
-    log.Fatal(afterFormat, " is not supported.")
-  }
+	if !isCorresponds(preFormat) {
+		log.Fatal(preFormat, " is not supported.")
+	} else if !isCorresponds(afterFormat) {
+		log.Fatal(afterFormat, " is not supported.")
+	}
 }
 
 func main() {
-  // fmt.Println("converting" ,preFormat, "to", afterFormat)
-  errHandler()
-  c := picconvert.NewPicConverter(flag.Args()[0], preFormat, afterFormat)
-  c.Conv()
+	// fmt.Println("converting" ,preFormat, "to", afterFormat)
+	errHandler()
+	c := picconvert.NewPicConverter(flag.Args()[0], preFormat, afterFormat)
+	c.Conv()
 }
--- kadai1/Mizushima/pic-conv/picconvert.go
+++ kadai1/Mizushima/pic-conv/picconvert.go
@@ -16,99 +16,103 @@ import (
 // a picture file format has root path, pre-conversion format,
 // and after-conversion format.
 type PicConverter struct {
-  Path        string
-  PreFormat   []string
-  AfterFormat string
+	Path        string
+	PreFormat   []string
+	AfterFormat string
 }
 
 // Conv converts picture file format.
 func (p *PicConverter) Conv() {
-  files, err := glob(p.Path, p.PreFormat)
+	files, err := glob(p.Path, p.PreFormat)
 
-  if err != nil {
-    log.Fatal("could not find the file path.");
-  }
+	if err != nil {
+		log.Fatal("could not find the file path.")
+	}
 
-  // prosessing for each files
-  for _, file := range files {
-    // fmt.Println("from:", file)
-    f, err := os.Open(file)
-    if err != nil {
-      fmt.Fprintln(os.Stderr, " the file could not open.");
-      os.Exit(1);
-    }
-    defer f.Close()
+	if files == nil {
+		log.Fatal("there's no image file.")
+	}
 
-    // reading the image file.
-    img, _, err := image.Decode(f)
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "Failed to convert image files.")
-      os.Exit(1)
-    }
+	// prosessing for each files
+	for _, file := range files {
+		// fmt.Println("from:", file)
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, " the file could not open.")
+			os.Exit(1)
+		}
+		defer f.Close()
 
-    // creating filepath for output.
-    output, err := os.Create(baseName(file) + 
-                            "_converted" +
-                            "." + 
-                            p.AfterFormat)
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "Failed to create the file.")
-      os.Exit(1)
-    }
+		// reading the image file.
+		img, _, err := image.Decode(f)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to convert image files.")
+			os.Exit(1)
+		}
 
-    // converting the file.
-    if p.AfterFormat == "png" {
-      err = png.Encode(output, img)
-    } else if p.AfterFormat == "jpg" || p.AfterFormat == "jpeg" {
-      err = jpeg.Encode(output, img, nil)
-    } else if p.AfterFormat == "gif" {
-      err = gif.Encode(output, img, nil)
-    }
+		// creating filepath for output.
+		output, err := os.Create(baseName(file) +
+			"_converted" +
+			"." +
+			p.AfterFormat)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to create the file.")
+			os.Exit(1)
+		}
 
-    if err != nil {
-      fmt.Fprintln(os.Stderr, err)
-      continue
-    }
-    
-    // fmt.Println("to:", output.Name())
-  }
+		// converting the file.
+		if p.AfterFormat == "png" {
+			err = png.Encode(output, img)
+		} else if p.AfterFormat == "jpg" || p.AfterFormat == "jpeg" {
+			err = jpeg.Encode(output, img, nil)
+		} else if p.AfterFormat == "gif" {
+			err = gif.Encode(output, img, nil)
+		}
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		fmt.Printf("converted %s\n", output.Name())
+	}
 }
 
 // NewPicConverter is the constructor for a PicConverter.
 func NewPicConverter(Path string, PreFormat string, AfterFormat string) *PicConverter {
-  res := new(PicConverter)
-  res.Path = Path
+	res := new(PicConverter)
+	res.Path = Path
 
-  if PreFormat == "jpeg" || PreFormat == "jpg" {
-    res.PreFormat = []string{"jpeg", "jpg"}
-  } else {
-    res.PreFormat = []string{PreFormat}
-  }
+	if PreFormat == "jpeg" || PreFormat == "jpg" {
+		res.PreFormat = []string{"jpeg", "jpg"}
+	} else {
+		res.PreFormat = []string{PreFormat}
+	}
 
-  res.AfterFormat = AfterFormat
-  return res
+	res.AfterFormat = AfterFormat
+	return res
 }
 
 // glob returns a slice of file path where format.
 func glob(path string, format []string) ([]string, error) {
-  var res []string
+	var res []string
 
-  var err error 
-  for _, f := range format {
-    err = filepath.Walk(path,
-      func(path string, info os.FileInfo, err error) error {
-        if filepath.Ext(path) == "." + f {
-          res = append(res, path)
-        }
-        return nil
-      })
-  }
+	var err error
+	for _, f := range format {
+		err = filepath.Walk(path,
+			func(path string, info os.FileInfo, err error) error {
+				if filepath.Ext(path) == "."+f {
+					res = append(res, path)
+				}
+				return nil
+			})
+	}
 
-  return res, err
+	return res, err
 }
 
 // baseName returns the filepath without a extension.
 func baseName(filePath string) string {
-  ext := filepath.Ext(filePath)
-  return filePath[:len(filePath)-len(ext)]
+	ext := filepath.Ext(filePath)
+	return filePath[:len(filePath)-len(ext)]
 }
