e6184a928a4527645dbb3a3a378061c8f5ec3425
Ryo Nakamine <rnakamine8080@gmail.com>
2021-06-11 01:31:18 +0900

    Add TestRun_parseError

---
+++ kadai2/rnakamine/cli_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestRun_parseError(t *testing.T) {
+	inStream := bytes.NewBufferString("")
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{inStream: inStream, outStream: outStream, errStream: errStream}
+	args := strings.Split("imgconv -foo bar", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeError {
+		t.Errorf("expected=%d, actual=%d", status, ExitCodeError)
+	}
+
+	expected := "flag provided but not defined: -foo"
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %s to contain %s", errStream.String(), expected)
+	}
+}
