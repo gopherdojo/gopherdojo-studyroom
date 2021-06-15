92b63c2ebe9bca49989c1141ebaf802dad851245
MizushimaToshihiko <pandachan5228@gmail.com>
2021-06-15 14:08:28 +0900

    時間制限とgo.mod追加

---
+++ kadai3-1/Mizushima/go.mod
@@ -0,0 +1,3 @@
+module github.com/MizushimaToshihiko/gopherdojo-studyroom/kadai3-1/Mizushima
+
+go 1.16
--- kadai3-1/Mizushima/main.go
+++ kadai3-1/Mizushima/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"math/rand"
@@ -11,16 +12,38 @@ import (
 
 func main() {
 
-	words := []string{ "ahoaho", "bakabaka", "unkounko" }
+	words := []string{"ahoaho", "bakabaka", "unkounko"}
 	rand.Seed(time.Now().UnixNano())
+
+	bc := context.Background()
+	t := 20 * time.Second
+	ctx, cancel := context.WithTimeout(bc, t)
+	defer cancel()
+
+	fmt.Println("> タイピングゲームを始めましゅ")
+	fmt.Println("> 英単語が出てきますので、同じ単語をタイプしてくだしゃい!")
+	fmt.Println("> 制限時間は20秒です")
+
 	ch := input(os.Stdin)
+	score := 0
+
 	for {
+
 		idx := rand.Intn(3)
 		fmt.Printf("> %s\n", words[idx])
-		if <-ch == words[idx] {
-			fmt.Println("> しぇいかい")
-		} else {
-			fmt.Println("> ぶっぶー")
+
+		select {
+		case <-time.After(1 * time.Second):
+			if <-ch == words[idx] {
+				fmt.Println("> しぇえか～い")
+				score++
+			} else {
+				fmt.Println("> ぶっぶー")
+			}
+		case <-ctx.Done():
+			fmt.Println("\n終了!")
+			fmt.Printf("%d問正解です!\n", score)
+			return
 		}
 	}
 }
