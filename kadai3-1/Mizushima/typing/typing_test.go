dc4d873d10d9a96e6603efee17d1f6329854e31a
MizushimaToshihiko <pandachan5228@gmail.com>
2021-07-07 14:00:09 +0900

    typing_test.goの変更 Test関数を追加 - TestGame_TimelimitOver - Test_input - Test_incSlice TestGame関数の名称変更=>TestGame_WithinTheTimelimit

--- kadai3-1/Mizushima/typing/typing_test.go
+++ kadai3-1/Mizushima/typing/typing_test.go
@@ -1,31 +1,28 @@
-package typing_test
+package typing
 
 import (
 	"bytes"
 	_ "embed"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
-
-	typing "github.com/MizushimaToshihiko/gopherdojo-studyroom/kadai3-1/Mizushima/typing"
 )
 
 //go:embed gamedata/words.csv
 var words string
 var wordsSlice = strings.Split(words, ",")
 
-func TestGame(t *testing.T) {
+func TestGame_WithinTheTimelimit(t *testing.T) {
 	t.Helper()
 
-	cases := []struct {
-		name     string
+	cases := map[string]struct {
 		tm       time.Duration
 		ans      []string
 		expected int
 	}{
-		{
-			name: "No typo",
-			tm:   3 * time.Second,
+		"No typo": {
+			tm:   1 * time.Second,
 			ans: []string{
 				"America",
 				"American",
@@ -40,9 +37,8 @@ func TestGame(t *testing.T) {
 			},
 			expected: 10,
 		},
-		{
-			name: "One typo",
-			tm:   3 * time.Second,
+		"One typo": {
+			tm:   1 * time.Second,
 			ans: []string{
 				"America",
 				"American",
@@ -57,17 +53,111 @@ func TestGame(t *testing.T) {
 			},
 			expected: 9,
 		},
+		"Two typo" : {
+			tm:   3 * time.Second,
+			ans: []string{
+				"America",
+				"American",
+				"Angle",
+				"April",
+				"August",
+				"Bacon",
+				"🤣",  // tyop 1
+				"Battery",
+				"👍",  // typo 2
+				"Bill",
+			},
+			expected: 8,
+		},
 	}
 
 	// []string{"America","American","Angle","April","August","Bacon","Barber","Battery","Bible","Bill"}
 	
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
 			output := new(bytes.Buffer)
 			input := bytes.NewBufferString(strings.Join(c.ans, "\n"))
-			actual, _ := typing.Game(input, output, wordsSlice, c.tm, true)
+			actual, _ := Game(input, output, wordsSlice, c.tm, true)
 			if actual != c.expected {
-				t.Errorf("wanted %d, but got %d", c.expected, actual)
+				t.Errorf("expected %d, but got %d", c.expected, actual)
+			}
+		})
+	}
+}
+
+func TestGame_TimelimitOver(t *testing.T) {
+	cases := map[string][]string {
+		"case 1" : {
+			"America",
+			"American",
+			"Angle",
+			"April",
+			"August",
+			"Bacon",
+			"Barber",
+			"Battery",
+			"Bible",
+			"Bill",
+		},
+	}
+
+	for name, val := range cases {
+		t.Run(name, func(t *testing.T) {
+			output := new(bytes.Buffer)
+			input := bytes.NewBufferString(strings.Join(val, "\n"))
+			actual, _ := Game(input, output, wordsSlice, time.Duration(1), true)
+			if actual > 0 {
+				fmt.Println(output.String())
+				t.Fatalf("expected 0, but got %d", actual)
+			}
+			if output.String() != fmt.Sprintf("> %s\n\nGame ends!\nThe number of correct answers is 0\n", val[0]) {
+				t.Fatal("message you got is not equal to expected")
+			}
+		})
+	}
+}
+
+func Test_input(t *testing.T) {
+	t.Helper()
+
+	cases := map[string]string {
+		"case 1" : "foo\nbar\nfoobar",
+		"case 2" : "foobarfoobar\nbar\nbar",
+	}
+
+	for name, val := range cases {
+		t.Run(name, func(t *testing.T) {
+			inputter := bytes.NewBufferString(val)
+			ch := input(inputter)
+			expects := strings.Split(val, "\n")
+
+			for _, e := range expects {
+				ans := <-ch
+				fmt.Println(ans)
+				if e != ans {
+					t.Fatalf("expected %s, but got %s", e, ans)
+				}
+			}
+		})
+	}
+}
+
+func Test_incSlice(t *testing.T) {
+	t.Helper()
+
+	cases := map[string][]int {
+		"case 1 n=5" : {0,1,2,3,4},
+		"case 2 n=10" : {0,1,2,3,4,5,6,7,8,9},
+	}
+
+	for name, val := range cases {
+		t.Run(name, func(t *testing.T) {
+			var i int
+			for _, v := range val {
+				if i != v {
+					t.Fatalf("expected %d, but got %d", i, v)
+				}
+				i++
 			}
 		})
 	}
