17e480f2d97315eed6dfa315cc0e9a40163c7124
Eiji Takahashi <>
2021-08-16 22:41:54 +0900

    フラグ構造体の修正　フラグの修正

--- kadai1/main.go
+++ kadai1/main.go
@@ -12,34 +12,34 @@ type flagStruct struct {
 	selectedDirecotry string
 	selectedFileType  string
 	convertedFileType string
+	stringPath []string
 }
 
 var flg flagStruct
 
-func returnFilePath(selectedFileType *string) ([]string, error) {
-	var stringPath []string
+func returnFilePath(selectedFileType *string) (error) {
 	err := filepath.Walk(flg.selectedDirecotry,
 		func(paths string, info fs.FileInfo, err error) error {
 			if filepath.Ext(paths) == *selectedFileType {
-				stringPath = append(stringPath, paths)
+				flg.stringPath = append(flg.stringPath, paths)
 			}
 			return nil
 		})
-	return stringPath, err
+	return err
 }
 
 func init() {
 
 	flag.StringVar(&flg.selectedDirecotry, "s", "", "ディレクトリを指定")
 	flag.StringVar(&flg.selectedFileType, "f", ".jpg", "変換前のファイルタイプを指定")
-	flag.StringVar(&flg.convertedFileType, "cf", ".jpg", "変換後のファイルタイプを指定")
+	flag.StringVar(&flg.convertedFileType, "cf", ".png", "変換後のファイルタイプを指定")
 
 }
 
 func main() {
 	flag.Parse()
-	paths, err := returnFilePath(&flg.selectedFileType)
-	fmt.Println(paths)
+	err := returnFilePath(&flg.selectedFileType)
+	fmt.Println(&flg.stringPath)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ディレクトリ選択をしてください。")
