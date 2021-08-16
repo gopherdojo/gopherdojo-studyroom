dd7f0e921bbdd7ee4854432b209f450a0042017d
Eiji Takahashi <>
2021-08-16 22:59:22 +0900

    タイポの修正

--- kadai1/main.go
+++ kadai1/main.go
@@ -9,16 +9,16 @@ import (
 )
 
 type flagStruct struct {
-	selectedDirectotry string
-	selectedFileType  string
-	convertedFileType string
-	stringPath        []string
+	selectedDirectory string
+	selectedFileType   string
+	convertedFileType  string
+	stringPath         []string
 }
 
 var flg flagStruct
 
 func returnFilePath(selectedFileType *string) error {
-	err := filepath.Walk(flg.selectedDirectotry,
+	err := filepath.Walk(flg.selectedDirectory,
 		func(paths string, info fs.FileInfo, err error) error {
 			if filepath.Ext(paths) == *selectedFileType {
 				flg.stringPath = append(flg.stringPath, paths)
@@ -29,13 +29,14 @@ func returnFilePath(selectedFileType *string) error {
 }
 func init() {
 
-	flag.StringVar(&flg.selectedDirectotry, "s", "", "ディレクトリを指定")
+	flag.StringVar(&flg.selectedDirectory, "s", "", "ディレクトリを指定")
 	flag.StringVar(&flg.selectedFileType, "f", ".jpg", "変換前のファイルタイプを指定")
 	flag.StringVar(&flg.convertedFileType, "cf", ".png", "変換後のファイルタイプを指定")
 
 }
 
 func main() {
+
 	flag.Parse()
 	err := returnFilePath(&flg.selectedFileType)
 	fmt.Println(&flg.stringPath)
