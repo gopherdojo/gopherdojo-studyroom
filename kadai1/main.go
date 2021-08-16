d1868b3847e713d0ca1b365ea52b943420fe3bbc
Eiji Takahashi <>
2021-08-16 22:34:49 +0900

    ファイルタイプの指定

--- kadai1/main.go
+++ kadai1/main.go
@@ -9,13 +9,14 @@ import (
 )
 
 var  selectedDirecotry string
+var  selectedFileType string
+var  convertedFileType string
 
-func returnFilePath() ([]string,error) {
+func returnFilePath(selectedFileType *string) ([]string,error) {
 	var stringPath  []string
-
 	err := filepath.Walk(selectedDirecotry,
 		func(paths string, info fs.FileInfo, err error) error {
-			if filepath.Ext(paths) == ".jpg" {
+			if filepath.Ext(paths) == *selectedFileType {
 				stringPath = append(stringPath,paths)
 			}
 			return nil
@@ -24,12 +25,17 @@ func returnFilePath() ([]string,error) {
 }
 
 func init()  {
+
 	flag.StringVar(&selectedDirecotry, "s", "", "ディレクトリを指定")
+	flag.StringVar(&selectedFileType, "f",".jpg", "変換前のファイルタイプを指定")
+	flag.StringVar(&convertedFileType, "cf",".jpg", "変換後のファイルタイプを指定")
+
 }
 
 func main() {
 	flag.Parse()
-	paths,err := returnFilePath()
+
+	paths,err := returnFilePath(&selectedFileType)
 	fmt.Println(paths)
 
 	if err != nil {
