f430aa947779022569aeb44530585ac90d79cc02
MizushimaToshihiko <pandachan5228@gmail.com>
2021-08-20 12:27:44 +0900

    modified main.go for first 'TRY' in day4-11.database

---
+++ kadai4/Mizushima/go.mod
@@ -0,0 +1,5 @@
+module github.com/MizushimaToshihiko/gopherdojo-studyroom/kadai4/Mizushima
+
+go 1.16
+
+require modernc.org/sqlite v1.12.0 // indirect
--- kadai4/Mizushima/main.go
+++ kadai4/Mizushima/main.go
@@ -1,47 +1,79 @@
 package main
 
 import (
-	"html/template"
+	"database/sql"
+	"fmt"
 	"log"
-	"math/rand"
-	"net/http"
-	"time"
+	"os"
+
+	_ "modernc.org/sqlite"
 )
 
-var res = map[int]string{
-	0: "大吉",
-	1: "中吉",
-	2: "中吉",
-	3: "小吉",
-	4: "小吉",
-	5: "凶",
-}
-
-type Person struct {
-	Name string `json:"name"`
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-
-	t := template.Must(template.New("msg").
-		Parse("<!DOCTYPE html><html><body>{{.Person}}さんの運勢は「<b>{{.Response}}</b>」です</body></html>"))
-
-	if err := t.Execute(w, struct {
-		Person   string
-		Response string
-	}{
-		Person:   r.FormValue("p"),
-		Response: res[rand.Intn(len(res))],
-	}); err != nil {
-		log.Fatalf("failed to execute template: %v", err)
-	}
-
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	db, err := sql.Open("sqlite", "database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Fprintln(os.Stderr, "connected to dakabase.db")
+	}
+	
 }
+
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"log"
+// 	"math/rand"
+// 	"net/http"
+// 	"time"
+// )
+
+// var res = map[int]string{
+// 	0: "大吉",
+// 	1: "中吉",
+// 	2: "中吉",
+// 	3: "小吉",
+// 	4: "小吉",
+// 	5: "凶",
+// }
+
+// type Person struct {
+// 	Name string `json:"name"`
+// }
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-type", "text/html; charset=utf-8")
+
+// 	t := template.Must(template.New("msg").
+// 		Parse("<!DOCTYPE html><html><body>{{.Person}}さんの運勢は「<b>{{.Response}}</b>」です</body></html>"))
+
+// 	if err := t.Execute(w, struct {
+// 		Person   string
+// 		Response string
+// 	}{
+// 		Person:   r.FormValue("p"),
+// 		Response: res[rand.Intn(len(res))],
+// 	}); err != nil {
+// 		log.Fatalf("failed to execute template: %v", err)
+// 	}
+
+// }
+
+// func main() {
+// 	rand.Seed(time.Now().UnixNano())
+
+// 	BootServer(handler)
+
+// }
+
+// func BootServer(handler func(http.ResponseWriter, *http.Request)) {
+
+// 	http.HandleFunc("/", handler)
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		fmt.Printf("BootServer: err: %s\n", err)
+// 	}
+
+// }
