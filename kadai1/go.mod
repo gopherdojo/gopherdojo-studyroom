fe705a9931de42376ab0a101b09090e7e672069d
HighPon <mjoppaida@icloud.com>
2021-08-29 23:25:28 +0900

    split main package

---
+++ kadai1/go.mod
@@ -0,0 +1,3 @@
+module highpon
+
+go 1.17
--- kadai1/main.go
+++ kadai1/main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"highpon/args"
 	"os"
 	"path/filepath"
 )
 
-type cmdArgs struct {
-	from string
-	to   string
-	dir  string
-}
-
-var cmd cmdArgs
-
-func init() {
-	flag.StringVar(&cmd.from, "from", "jpg", "from")
-	flag.StringVar(&cmd.to, "to", "png", "to")
-	flag.StringVar(&cmd.dir, "dir", "./", "directory")
-}
-
 func main() {
-	flag.Parse()
-	fmt.Println(cmd.from, cmd.to, cmd.dir)
-
+	var a = args.ParseArgs()
+	fmt.Println(a.From)
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
