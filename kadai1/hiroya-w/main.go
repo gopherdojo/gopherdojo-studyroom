b5aee43a10473c03a631920108107d9cb9f948b3
Hiroya-W <hyuyukun@gmail.com>
2021-09-17 22:36:39 +0900

    :+1: Validate inputType, outputType

--- kadai1/hiroya-w/main.go
+++ kadai1/hiroya-w/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"imgconv/imgconv"
 	"log"
 )
 
 var (
 	inputType  string
 	outputType string
+	directory  string
 )
 
 func init() {
@@ -16,11 +18,38 @@ func init() {
 	flag.StringVar(&outputType, "output-type", "png", "output type")
 }
 
+func validateType(t string) error {
+	switch t {
+	case "jpg":
+		fallthrough
+	case "png":
+		return nil
+	default:
+		return fmt.Errorf("invalid type: %s", t)
+	}
+}
+
 func validateArgs() error {
 	flag.Parse()
+
+	if inputType == outputType {
+		return fmt.Errorf("input and output type can't be the same")
+	}
+
+	if err := validateType(inputType); err != nil {
+		return err
+	}
+
+	if err := validateType(outputType); err != nil {
+		return err
+	}
+
 	if flag.Arg(0) == "" {
 		return fmt.Errorf("directory is required")
+	} else {
+		directory = flag.Arg(0)
 	}
+
 	return nil
 }
 
