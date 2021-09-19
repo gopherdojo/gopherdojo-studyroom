8a189cca1bb1aff7e7c77f20779ceca3bdf2f429
Hiroya-W <hyuyukun@gmail.com>
2021-09-19 11:34:52 +0900

    :sparkles: Create convert function

--- kadai1/hiroya-w/imgconv/imgconv.go
+++ kadai1/hiroya-w/imgconv/imgconv.go
@@ -2,6 +2,10 @@ package imgconv
 
 import (
 	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"os"
 	"path/filepath"
 )
@@ -13,7 +17,9 @@ func Converter(directory, inputType, outputType string) error {
 	}
 
 	for _, path := range imgPaths {
-		fmt.Println(path)
+		if err := convert(path, outputType); err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -47,3 +53,54 @@ func getFiles(directory, inputType string) ([]string, error) {
 	return imgPaths, nil
 }
 
+func convert(filePath, outputType string) error {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+
+	fileName := filepath.Base(filePath)
+	output, err := os.Create(fileName + "." + outputType)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	switch outputType {
+	case "jpg", "jpeg":
+		return convertJPG(img, output)
+	case "png":
+		return convertPNG(img, output)
+	case "gif":
+		return convertGIF(img, output)
+	default:
+		return fmt.Errorf("%s is not a supported output type", outputType)
+	}
+}
+
+func convertJPG(img image.Image, output *os.File) error {
+	if err := jpeg.Encode(output, img, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+func convertPNG(img image.Image, output *os.File) error {
+	if err := png.Encode(output, img); err != nil {
+		return err
+	}
+	return nil
+}
+
+func convertGIF(img image.Image, output *os.File) error {
+	if err := gif.Encode(output, img, nil); err != nil {
+		return err
+	}
+	return nil
+}
