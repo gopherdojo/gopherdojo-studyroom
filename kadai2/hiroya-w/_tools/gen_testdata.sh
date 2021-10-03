9e7137bb9cd22fc001fbaa29f8263811ada00aed
Hiroya-W <hyuyukun@gmail.com>
2021-10-03 17:07:08 +0900

    :rotating_light: test-deps ターゲットを追加

--- kadai2/hiroya-w/_tools/gen_testdata.sh
+++ kadai2/hiroya-w/_tools/gen_testdata.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 curl https://avatars.githubusercontent.com/hiroya-w -o hiroya-w.png
 
-for i in {1..10}
+for i in {1..2}
 do
-    cp hiroya-w.png image$i.png
+    cp hiroya-w.png ../testdata/image$i.png
 done
 
 rm hiroya-w.png
