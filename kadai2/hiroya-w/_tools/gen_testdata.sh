63a940f1e34cad9b3bdd054be6caffea3c689c6f
Hiroya-W <hyuyukun@gmail.com>
2021-10-04 16:33:33 +0900

    :wrench: テストデータの生成スクリプトを修正

--- kadai2/hiroya-w/_tools/gen_testdata.sh
+++ kadai2/hiroya-w/_tools/gen_testdata.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-curl https://avatars.githubusercontent.com/hiroya-w -o hiroya-w.png
+DIR=${1:-.}
 
-for i in {1..2}
-do
-    cp hiroya-w.png ../testdata/image$i.png
-done
-
-rm hiroya-w.png
+curl https://avatars.githubusercontent.com/hiroya-w -o $DIR/image_png.png
+curl http://icb-lab.naist.jp/members/yoshi/ouec_lecture/image_recognition/image_files/lena.jpg -o $DIR/image_jpg.jpg
+curl https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif -o $DIR/image_gif.gif
