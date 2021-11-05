cdcefc3d95ebdc220980587d4687c831cfa3b857
Saza-ku <the.latticeheart@gmail.com>
2021-11-05 13:45:20 +0900

    入力検証を追加

--- kadai1/saza/main.go
+++ kadai1/saza/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/saza-ku/gopherdojo-studyroom/kadai1/saza/imgconv"
 )
@@ -10,15 +12,56 @@ var (
 	root string
 	src string
 	dest string
+
+	types = []string{"jpg", "png", "gif"}
 )
 
+
 func main() {
+	s := flag.String("s", "jpg", "source image type")
+	d := flag.String("d", "png", "destination image type")
 	flag.Parse()
+
 	root = flag.Args()[0]
-	src = *flag.String("s", "jpeg", "source image type")
-	dest = *flag.String("d", "png", "destination image type")
+	src = *s
+	dest = *d
+
+	if valid, message :=validateInput(); !valid {
+		fmt.Println("Error: " + message)
+		return
+	}
 
 	c := imgconv.NewConverter(root, src, dest)
 
 	c.Run()
+}
+
+func validateInput() (bool, string) {
+	// check whether root exists
+	if f, err := os.Stat(root); err != nil || !f.IsDir() {
+		return false, fmt.Sprintf("directory %s doesn't exists", root)
+	}
+
+	// check image types
+	valid := false
+	for _, t := range types {
+		if src == t {
+			valid = true
+		}
+	}
+	if !valid {
+		return false, fmt.Sprintf("image type %s is invalid", src)
+	}
+
+	valid = false
+	for _, t := range types {
+		if dest == t {
+			valid = true
+		}
+	}
+	if !valid {
+		return false, fmt.Sprintf("image type %s is invalid", src)
+	}
+
+	return true, ""
 }
